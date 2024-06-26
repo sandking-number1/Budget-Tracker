fed478c8d3b8d516bf155f12c26d4fd516ee25a5
sandking-number1 <Alfred.J9412@gmail.com>
2024-06-26 13:30:14 -0400

    Connected to server

--- config/connectDb.js
+++ config/connectDb.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const connectDb = async () => {
   try {
-    await mongoose.connect("mongodb+srv://srirajuppalapati9:sriraj@cluster0.9if4yk1.mongodb.net/Cluster0");
+    await mongoose.connect(
+      "mongodb+srv://srirajuppalapati9:sriraj@cluster0.9if4yk1.mongodb.net/Cluster0"
+    );
     console.log(`Server Running On ${mongoose.connection.host}`);
   } catch (error) {
     console.log(`${error}`);
